0d252ac12cb1a585aa33c6f9cdb6dd9c96427f61
LuuLeThaiSon <llts28497@gmail.com>
2022-06-17 23:27:02 +0900

    main

--- so-chan/index.js
+++ so-chan/index.js
@@ -1,9 +1,9 @@
 function check() {
      let num = +document.getElementById("num").value;
-     
+
      if (num%2 == 0) {
-          document.getElementById("result").innerHTML = "So chan";
+          document.getElementById("result").innerHTML =num + " la so chan";
      } else {
-          document.getElementById("result").innerHTML = "So le";
+          document.getElementById("result").innerHTML =num + " la so le";
      }
 }
